8da8f91dc624700a1807fce2b5a958a20bd1d00c
ridefox-ahirani <asimm.hirani@gmail.com>
2021-10-17 12:07:08 -0400

    Added logic to work for packet offset corruption in receiver

--- data-transfer-py/data_transfer.py
+++ data-transfer-py/data_transfer.py
@@ -2,25 +2,33 @@ import serial
 import struct
 import pandas as pd
 from datetime import datetime
+import zlib
 import h5py
 
 portName = 'COM5'
 structString = '!IIIB'
 structSize = struct.calcsize(structString)
-print(structSize)
 HEADER = 0xFFCCFFCC
 
 def main():
+    packetCorrupt = 0
+    packetGood = 0
     ser = serial.Serial(portName)
     ser.write(str.encode('\x00')) # write one byte to start the daq
     start_time = datetime.now()
+    # ser.read(4) # intentionally insert offset
     while True:
         try:
             byte_arr = ser.read(structSize)
             data_recv = struct.unpack(structString,byte_arr)
             if (data_recv[0] != HEADER):
+                print('Corrupt Packet!')
+                packetCorrupt = packetCorrupt+1
+                ser.read_until(expected=HEADER.to_bytes(4,'big')) # discard until the next header
+                ser.read(structSize-4) # discard bytes of the next packet
                 continue
-            print('Header: ' + hex(data_recv[0]) + ' Data: ' + hex(data_recv[1]) + ' Time: ' + str(data_recv[2]) + ' CRC: ' + hex(data_recv[3]))
+            packetGood = packetGood+1
+            print('Header: ' + hex(data_recv[0]) + ' Data: ' + hex(data_recv[1]) + ' Time: ' + str(data_recv[2]) + ' CRC: ' + hex(data_recv[3]) + ' Good Packet: ' + str(packetGood) + ' Bad Packet: ' + str(packetCorrupt))
         except (KeyboardInterrupt):
             break
 
