bde23a5ebbb65abfacf04e8ddfb7a7398b190688
Alice Barbe <alice_barbe@hotmail.com>
2022-02-08 06:50:03 -0500

    get start/end time buttons working

--- Chard/app/src/main/java/com/example/chard/AddEvent.java
+++ Chard/app/src/main/java/com/example/chard/AddEvent.java
@@ -9,18 +9,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class AddEvent extends AppCompatActivity {
 
     Button startTimeButton;
-    int startHour, startMin;
     Button endTimeButton;
-    int endHour, endMin;
     Switch instantaneousSwitch;
+    private int startHour, startMin;
+    private int endHour, endMin;
+    private boolean startChanged = false;
+    private boolean endChanged = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +33,10 @@ public class AddEvent extends AppCompatActivity {
         setContentView(R.layout.activity_add_event);
         startTimeButton = findViewById(R.id.buttonStartTime);
         endTimeButton = findViewById(R.id.buttonEndTime);
-        instantaneousSwitch = findViewById(R.id.switchInstantaneous);
-        instantaneousSwitch.setChecked(false);
-
-        endTimeButton.setVisibility(View.VISIBLE);
+        //instantaneousSwitch = findViewById(R.id.switchInstantaneous);
+        /*
         instantaneousSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // remove end time button if instantaneous switch is checked
                 if (isChecked) {
@@ -41,7 +45,7 @@ public class AddEvent extends AppCompatActivity {
                     endTimeButton.setVisibility(View.VISIBLE);
                 }
             }
-        });
+        });*/
     }
 
     public void popStartTimePicker(View view) {
@@ -51,6 +55,11 @@ public class AddEvent extends AppCompatActivity {
                 startHour = selectedHour;
                 startMin = selectedMinute;
                 startTimeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", startHour, startMin));
+                startChanged = true;
+
+                endTimeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", startHour, startMin));
+                endHour = startHour;
+                endMin = startMin;
             }
         };
 
@@ -64,9 +73,9 @@ public class AddEvent extends AppCompatActivity {
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 endHour = selectedHour;
-                endHour = selectedMinute;
+                endMin = selectedMinute;
                 endTimeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", endHour, endMin));
-
+                endChanged = true;
             }
         };
 
@@ -79,10 +88,29 @@ public class AddEvent extends AppCompatActivity {
         view.findViewById(R.id.buttonSubmit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("dbtest",  startHour + " " + endHour);
+                EditText editTextTitle = (EditText) findViewById(R.id.editTextTitle);
+                String eventTitle = editTextTitle.getText().toString();
+                if (eventTitle.matches("")) {
+                    Toast.makeText(view.getContext(), "You did not enter an event title", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (!startChanged){
+                    Toast.makeText(view.getContext(), "You did not enter a start time", Toast.LENGTH_SHORT).show();
+                    return;
+                } /*else if (!endChanged) {
+                    if (instantaneousSwitch.isChecked()) {
+                        endHour = startHour;
+                        endMin = startMin;
+                    } else {
+                        Toast.makeText(view.getContext(), "You did not enter an end time", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }*/
+
+                Log.d("dbtest", eventTitle + " " + startHour + ":" + startMin
+                        + " " + endHour + ":" + endMin);
+
             }
         });
     }
-
 
 }
